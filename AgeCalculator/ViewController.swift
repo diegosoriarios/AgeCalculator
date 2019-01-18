69cb6789c3c5d7660e8543540130afecc416dfed
diego.rios <diego.rios@CTNAPPLE01YG8WN.local>
2019-01-18 17:38:04 -0200

    Initial Commit

---
+++ AgeCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AgeCalculator
+//
+//  Created by diego.rios on 18/01/19.
+//  Copyright © 2019 diego.rios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AgeCalculatorTests/AgeCalculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  AgeCalculatorTests.swift
+//  AgeCalculatorTests
+//
+//  Created by diego.rios on 18/01/19.
+//  Copyright © 2019 diego.rios. All rights reserved.
+//
+
+import XCTest
+@testable import AgeCalculator
+
+class AgeCalculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
