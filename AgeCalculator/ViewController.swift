721e9625e1a66075a2f1c2c509e496f6aeaacba9
diegosoriarios <diegosoriarios@gmail.com>
2019-03-07 15:58:28 -0300

    first commit

--- AgeCalculator/ViewController.swift
+++ AgeCalculator/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblAge: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func btnCalculateHandler(_ sender: UIButton){
+        let birthDate = self.datePicker.date
+        let today = Date()
+        if birthDate >= today {
+            let alertView = UIAlertController(title: "Error", message: "Please enter a valid date", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alertView.addAction(action)
+            self.present(alertView, animated: false, completion: nil)
+            return
+        }
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month, .day], from: birthDate, to: today)
+        let ageYears = components.year
+        let ageMonths = components.month
+        let ageDays = components.day
+        
+        self.lblAge.text = "\(ageYears!) years, \(ageMonths!) months, \(ageDays!) days"
+    }
 
 }
 
